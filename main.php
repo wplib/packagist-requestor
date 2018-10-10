8ab2f0907947a60ff3245edd0e5c47701a91dd53
Mike Schinkel <mike@newclarity.net>
2018-10-10 03:32:33 -0400

    Improved some comments, added some helpful echos.

--- main.php
+++ main.php
@@ -25,12 +25,17 @@ do {
 			}
 		}
 	} catch ( Exception $e ) {
+		/**
+		 * Wait a minute after an error.
+		 */
+		echo "\n\nSleeping for a minute.\n\n";
 		sleep( 60 );
 		continue;
 	}
 	/**
-	 * Wait an hour after finishing
+	 * Wait an hour after processing the full list.
 	 */
+	echo "\n\nSleeping for an hour.\n\n";
 	sleep( 60 * 60 );
 } while ( true );
 
